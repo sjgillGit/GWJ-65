91df953d0eb7e14b615aaa6baefba3de4737050c
Jakfusco <55301117+Jakfusco@users.noreply.github.com>
2024-01-21 15:30:27 -0500

    Built scene skeleton for game starting from menu

---
+++ Scenes/Opertator Tutorial/Operator Tutorial Assets/Scripts/title screen.gd
@@ -0,0 +1,43 @@
+extends Control
+
+
+func _on_operator_start_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_roamer_start_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_operator_tutorial_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_roamer_tutorial_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	
+	
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed(): #Shockingly, quits the game
+	
+	
+	pass # Replace with function body.
